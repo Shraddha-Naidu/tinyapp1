21c8c85d3e13bc423bb8d47bd177f5ee1aacb5f3
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-03-24 01:26:08 -0600

    Reorganized Routes

--- express_server.js
+++ express_server.js
@@ -59,14 +59,26 @@ app.get("/urls", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  console.log(req.body); 
+  res.send("Ok");
 });
 
 //Retrieves short urls
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
+});
+
+app.post("/urls/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls/${shortURL");
 });
 
 //Route to new URLs
@@ -78,8 +90,6 @@ app.post("/urls/new", (req, res) => {
   console.log(req.body);  // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
-
-
 
 
 //Removes deleted URL
