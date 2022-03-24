3779ce22553daddf80f004cbd4f430da478bdfee
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-03-24 01:06:44 -0600

    Updated GET to redirect to "/u/:shortURL" - longUR

--- express_server.js
+++ express_server.js
@@ -52,8 +52,10 @@ app.get("/urls.json", (req, res) => {
 
 //Retrieves urls
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+  const longURL = req.body.longURL;
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls/${shortURL}");
 });
 
 app.post("/urls", (req, res) => {
@@ -62,9 +64,9 @@ app.post("/urls", (req, res) => {
 });
 
 //Retrieves short urls
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 //Route to new URLs
