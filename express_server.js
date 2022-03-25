0549d0c31c8dee24a83944685d6cc283784413ae
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-03-24 19:42:18 -0600

    Added cookie-parser

--- express_server.js
+++ express_server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const app = express();
+const cookieParser = require('cookie-parser')
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
