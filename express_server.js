febf32cad7bdc6bbbff9a6f44021f37e902ca97c
Shraddha-Naidu <naidu.shraddha@gmail.com>
2022-03-25 01:09:18 -0600

    File created to separate helper functions

--- express_server.js
+++ express_server.js
@@ -40,26 +40,7 @@ const userDatabase = {
 
 /* FUNCTIONS */
 
-//Checks for existing user
-const existingUser = function(email, userDatabase) {
-  for (const user in userDatabase) {
-    if (userDatabase[user].email === email) {
-      return userDatabase[user].id;
-    }
-  }
-  return false;
-};
-
-//Creates a random string, used to create short URLs/userIDs
-const generateRandomString = function() {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
-  let randomString = "";
-  while (randomString.length < 6) {
-    randomString = chars[Math.floor(Math.random() * chars.length)];
-  }
-  return randomString;
-};
-
+const { existingUser, generateRandomString } = require("./helpers");
 
 /* ROUTES */
 
